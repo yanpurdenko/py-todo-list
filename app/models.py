47c400a43c57557b5703a86187bdc927c957e59c
Yan Purdenko <purdenko22@gmail.com>
2023-02-17 23:44:43 +0100

    rename created_date to created_at, is_completed to is_done and add class Meta in models

--- app/models.py
+++ app/models.py
@@ -1,16 +1,25 @@
 from django.db import models
 
 
-class Task(models.Model):
-    title = models.CharField(max_length=255, null=False, blank=False)
-    created_date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    deadline = models.DateField(blank=True, null=True)
-    is_completed = models.BooleanField(default=False)
-
-    def __str__(self):
-        return self.title
-
-
 class Tag(models.Model):
     name = models.CharField(max_length=255)
-    task = models.ManyToManyField(Task)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateField(blank=True, null=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["is_done", "created_at"]
+
+    def __str__(self):
+        return f"{self.title}"
