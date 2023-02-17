63a6312df2a2c6ea953ede8766d6f4a25ae5f4f1
Yan Purdenko <purdenko22@gmail.com>
2023-02-17 19:58:42 +0100

    create index view

--- app/views.py
+++ app/views.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_visits": num_visits + 1,
+
+    }
+
+    return render(request, "app/index.html", context=context)
+
+
+
