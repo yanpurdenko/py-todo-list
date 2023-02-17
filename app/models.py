26cf6b4a623352de926b2239c717746a27aa4425
Yan Purdenko <purdenko22@gmail.com>
2023-02-17 19:49:09 +0100

    create models and make migration

--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255, null=False, blank=False)
+    created_date = models.DateTimeField(auto_now_add=True, null=False, blank=False)
+    deadline = models.DateField(blank=True, null=True)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    task = models.ManyToManyField(Task)
